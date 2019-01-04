99c11e59b80d3703a62212292761e52dcf659596
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2019-01-04 16:56:59 -0200

    Cria novo banco de dados Cadastro e tabela Pessoas

--- consultas.sql
+++ consultas.sql
@@ -5,3 +5,17 @@ Objetivo sucinto do arquivo: Arquivo criado para testes e aprendizado de SQL
 Referência: https://www.youtube.com/playlist?list=PLHz_AreHm4dkBs-795Dsgvau_ekxg8g1r
 Informações adicionais:
 */
+
+create database Cadastro;
+use Cadastro;
+
+create table Pessoas(
+	 nome varchar(30),
+     idade tinyint(3),
+     sexo char(1),
+     peso float,
+     altura float,
+     nacionalidade varchar(20)
+);
+
+describe Pessoas;
