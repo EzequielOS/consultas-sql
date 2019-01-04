d40a7bbe0035271d601425cea8aad52ba310a6fb
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2019-01-04 15:47:59 -0200

    Cria arquivo inicial

---
+++ consultas.sql
@@ -0,0 +1,7 @@
+/*
+Nome do autor: Ezequiel de Oliveira Sant'Ana
+Data de criação do arquivo: 04/01/2019
+Objetivo sucinto do arquivo: Arquivo criado para testes e aprendizado de SQL
+Referência: https://www.youtube.com/playlist?list=PLHz_AreHm4dkBs-795Dsgvau_ekxg8g1r
+Informações adicionais:
+*/
