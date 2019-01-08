7d729f55b660974825957b349d42bf4b0f6e4d55
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2019-01-08 15:09:09 -0200

    Implementa comando INSERT INTO

--- consultas.sql
+++ consultas.sql
@@ -23,3 +23,10 @@ create table Pessoas(
     nacionalidade varchar(20) default "Brasil",
     primary key (id)
 )default charset = utf8;
+
+delete into Pessoas values
+(default, 'Claudio', '1945-4-22', 'M', '99.0', '2.15', 'Brasil'),
+(default, 'Pedro', '1999-12-3', 'M', '87', '2', default),
+(default, 'Janaina', '1987-11-12', 'F', '75.4', '1.66', 'EUA');
+
+select * from Pessoas;
