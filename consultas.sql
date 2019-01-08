229e09d2d00da9fbf2acf8e570d82a83db375a52
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2019-01-08 17:10:44 -0200

    Insere comandos DDL na tabela Pessoas

--- consultas.sql
+++ consultas.sql
@@ -6,27 +6,40 @@ Referência: https://www.youtube.com/playlist?list=PLHz_AreHm4dkBs-795Dsgvau_ekx
 Informações adicionais:
 */
 
-#drop database Cadastro;  //deleta um DB
+desc Pessoas;
 
-create database Cadastro
-default character set utf8
-default collate utf8_general_ci;
-use Cadastro;
+alter table Pessoas
+rename to gafanhotos;/*renomea a tabela*/
 
-create table Pessoas(
-	id int not null auto_increment,
-	nome varchar(30) not null,
-    nascimento date,
-    sexo enum('M','F'),
-    peso decimal(5,2),
-    altura decimal(3,2),
-    nacionalidade varchar(20) default "Brasil",
-    primary key (id)
-)default charset = utf8;
+alter table Pessoas
+add column profissao varchar(10) after nome;/*adiciona na proxima posicao*/
+/*add codigo int first; //adiciona coluna na 1a posição*/
+/*add column profissao varchar(10); //adiciona na ultima posicao*/
 
-delete into Pessoas values
-(default, 'Claudio', '1945-4-22', 'M', '99.0', '2.15', 'Brasil'),
-(default, 'Pedro', '1999-12-3', 'M', '87', '2', default),
-(default, 'Janaina', '1987-11-12', 'F', '75.4', '1.66', 'EUA');
+alter table Pessoas
+/*drop column profissao; //exclui coluna no alter table*/
+modify column profissao varchar(20) not null default '';
+
+alter table Pessoas
+change profissao prof varchar(20);/*renomea coluna*/
 
 select * from Pessoas;
+
+
+/*-------------------------- Tabela  Cursos -------------------------------------------*/
+
+create table if not exists cursos(
+nome varchar (30) not null unique,
+descricao text,
+carga int unsigned,
+totaulas int unsigned,
+ano year default '2016'
+) default charset=utf8;
+
+alter table cursos
+add idcurso int first;
+
+alter table cursos
+add primary key (idcurso);
+
+select * from cursos;
